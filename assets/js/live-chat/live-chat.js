4c03a3dd0672b4cdc91e9de3ce4210640b97c2b5
Sumanto47 <sumantochatterjeenavi47@gmail.com>
2024-05-14 16:44:50 +0530

    Add files via upload

---
+++ assets/js/live-chat/live-chat.js
@@ -0,0 +1,11 @@
+function openForm() {
+   document.getElementById("myForm").style.visibility = "visible";
+   document.getElementById("myForm").style.opacity = "1";
+   document.getElementById("myForm").style.top = "5%";
+}
+ 
+function closeForm() {
+   document.getElementById("myForm").style.visibility = "hidden";
+   document.getElementById("myForm").style.opacity = "0";
+   document.getElementById("myForm").style.top = "0";
+}
